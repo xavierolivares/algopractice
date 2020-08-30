907e5be2e2a7c7b5e349a8ca67a94493168ec56e
Xavier Olivares <xm.olivares@gmail.com>
2020-08-29 22:07:49 -0400

    same

--- ColtSteele/same.js
+++ ColtSteele/same.js
@@ -1,21 +1,23 @@
-/* Write a function called same, which accepts two arrays. The function should return true if every value in the array has its corresponding value squared in the second array. The frequency of values must be the same. */
-
 function same(arr1, arr2) {
     var first = arr1.sort();
     var second = arr2.sort();
     var len = second.length;
-
+    var count = 0;
+    console.log('first', first)
+    console.log('second', second)
     if (arr1.length !== arr2.length) {
         return false
     }
 
-    for (let i = len-1; i > 0; i--) {
-        var sq = first[i] * 2;
+    for (let i = 0; i < len; i++) {
+        var sq = first[i] * first[i];
         if (sq === second[i]) {
-            first.pop();
-            second.pop();
+          count++;
         }
     }
+    console.log('count', count)
+    if (first.length === count) return true;
+    return false;
     // compare the arrays
     // if a squared number doesnt exist in arr2, return false
     // i need to consider the frequency as well
@@ -24,9 +26,8 @@ function same(arr1, arr2) {
     // if the length of each array is 0 by the end, return true. else, return false
 }
 
-
 // examples
-same([2,2,2], [4,4,4]) // returns true
-same([1], [4]) // returns false
-same([1,1,2], [4,1,1]) // returns true
-same([1], [])
+console.log(same([2,2,2], [4,4,4])) // returns true
+console.log(same([1], [4])) // returns false
+console.log(same([1,1,2], [4,1,1])) // returns true
+console.log(same([1], [])) // returns false
