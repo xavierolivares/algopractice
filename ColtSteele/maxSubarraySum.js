96d3f0ca4b02ea23ed2370a6b99b7603173ed09f
Xavier Olivares <xm.olivares@gmail.com>
2020-08-30 22:14:51 -0400

    maxSubarraySum

---
+++ ColtSteele/maxSubarraySum.js
@@ -0,0 +1,22 @@
+function maxSubarraySum(arr, num) {
+    let sum = 0;
+    let max = -Infinity;
+
+    for (let i = 0; i < num; i++) {
+        sum += arr[i];
+    }
+
+    for (let i = 0; i < arr.length-num; i++) {
+        console.log('sumpre', arr[num+i])
+        let windowsEnd = num+i;
+        sum -= arr[i];
+        sum += arr[windowsEnd];
+        console.log('sumpost', sum)
+        if (sum > max) {
+            max = sum;
+        }
+    }
+    return max;
+}
+
+console.log(maxSubarraySum([1,2,3,4,5,5,3,2,3,4,55,3,2], 3))
